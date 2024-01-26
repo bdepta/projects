a0d4440956580a66c3944387b30721331d2a6868
Bartek <b.depta@icloud.com>
2024-01-26 21:14:16 +0100

    Day34 - Completed

--- python/100-days-of-code/Day34/main.py
+++ python/100-days-of-code/Day34/main.py
@@ -4,8 +4,6 @@ from ui import QuizInterface
 import requests
 
 
-ui = QuizInterface()
-
 response = requests.get(url="https://opentdb.com/api.php?amount=10&category=18&type=boolean")
 question_bank = []
 question_data = response.json()["results"]
@@ -16,11 +14,6 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-ui.update_quiz(quiz.score,quiz.q_text)
-
+quiz_ui = QuizInterface(quiz)
 # while quiz.still_has_questions():
-#     quiz.next_question()
-
-# print("You've completed the quiz")
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-ui.mainloop()
+#     quiz_ui.get_next_question()
--- python/100-days-of-code/Day34/ui.py
+++ python/100-days-of-code/Day34/ui.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional, Tuple, Union
+from quiz_brain import QuizBrain
 import customtkinter as ctk
 from PIL import Image
 from customtkinter.windows.widgets.font import CTkFont
@@ -6,28 +7,49 @@ from customtkinter.windows.widgets.image import CTkImage
 THEME_COLOR = "#375362"
 
 class QuizInterface(ctk.CTk):
-    def __init__(self, **kwargs):
+    def __init__(self, quiz_brain: QuizBrain,**kwargs):
         super().__init__(fg_color=THEME_COLOR, **kwargs)
+        self.quiz = quiz_brain
         self.title("Quiz IT!")
         self.geometry("500x900")
         self.grid_columnconfigure((0,1), weight=1)
         self.resizable(False,False)
-        self.score = 0
-        self.question = ""
-        self.score_label = ctk.CTkLabel(master=self, text=f"Score: {self.score}/10", font=("courier new",40,"italic"), text_color="white")
+        self.score_label = ctk.CTkLabel(master=self, text=f"Score: 0/10", font=("courier new",40,"italic"), text_color="white")
         self.score_label.grid(row=0, column=0, columnspan=2, sticky="e", padx=(0,30), pady=(30,0))
-        self.question_area = ctk.CTkLabel(master=self, text=f"{self.question}", width=200, height=400, fg_color="white", text_color="black", font=("courier new", 20,"bold"), wraplength=300)
+        self.question_area = ctk.CTkLabel(master=self, text=f"", width=200, height=400, fg_color="white", text_color="black", font=("courier new", 20,"bold"), wraplength=300)
         self.question_area.grid(row=1,column=0, sticky="we", columnspan=2, padx=50, pady=70)
-        self.red_button = Button(master=self, button_img="./images/false.png", fg_color=THEME_COLOR, text="",hover = False)
+        self.red_button = Button(master=self, button_img="./images/false.png", fg_color=THEME_COLOR, text="",hover = False, command=self.red_button_action)
         self.red_button.grid(row=3,column=0, sticky="nesw",pady=(40,0))
-        self.green_button = Button(master=self, button_img="./images/true.png", fg_color=THEME_COLOR, text="",hover = False)
+        self.green_button = Button(master=self, button_img="./images/true.png", fg_color=THEME_COLOR, text="",hover = False, command=self.green_button_action)
         self.green_button.grid(row=3, column=1, sticky="nesw",pady=(40,0))
+        self.get_next_question()
+        self.mainloop()
     
-    def update_quiz(self, score, question):
-        self.score = score
-        self.question = question
-        self.score_label.configure(text=f"Score: {self.score}/10")
-        self.question_area.configure(text=f"{self.question}")
+    def get_next_question(self):
+        self.question_area.configure(fg_color="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            score = self.quiz.score
+            self.question_area.configure(text=f"{q_text}")
+            self.score_label.configure(text=f"Score: {score}/10")
+        else:
+            self.question_area.configure(text="You've reached the end of quiz. Check your score in top right corner.")
+            self.red_button.configure(command=None)
+            self.green_button.configure(command=None)
+    def green_button_action(self):
+        self.feedback(self.quiz.check_answer(user_answer="True"))
+    
+    def red_button_action(self):
+        is_right = self.quiz.check_answer(user_answer="False")
+        self.feedback(is_right)
+
+    def feedback(self, is_right):
+        if is_right:
+            self.question_area.configure(fg_color="green")
+        else:
+            self.question_area.configure(fg_color="red")
+        self.after(1000, self.get_next_question)
+        
 
 class Button(ctk.CTkButton):
     def __init__(self, button_img, width=100, height=99,**kwargs):
